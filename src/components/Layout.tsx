1752b15c2068cbbdd9e702720387b777bc4fd99e
Dima163163 <khlopovdima@gmail.com>
2026-02-28 11:40:42 +0400

    initial commit

---
+++ src/components/Layout.tsx
@@ -0,0 +1,22 @@
+import {Link, Outlet} from "react-router-dom";
+
+export const Layout = () => {
+    return (
+        <>
+            <header>
+                <h1>Header!</h1>
+                <nav style={{display: 'flex', gap: '30px'}}>
+                    <Link to={'/'}>Home</Link>
+                    <Link to={'/about'}>About</Link>
+                    <Link to={'/counter'}>Counter</Link>
+                    <a target='_blank' href={import.meta.env.VITE_API_URL}>Google</a>
+                    <a target='_blank' href={import.meta.env.SECRET_URL}>Ya</a>
+                </nav>
+            </header>
+            <main style={{flex: '1'}}>
+                <Outlet/>
+            </main>
+            <footer>Footer</footer>
+        </>
+    )
+}
